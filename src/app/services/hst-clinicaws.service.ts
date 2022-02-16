1fde65775e90fb6a9240f2d7d44b90da05634bc3
ddutan2000 <adriandutan15@gmail.com>
2022-02-15 23:37:28 -0500

    modificacion de servicio clinica

--- src/app/services/hst-clinicaws.service.ts
+++ src/app/services/hst-clinicaws.service.ts
@@ -11,25 +11,31 @@ export class HstClinicawsService {
 
   findAllCitas(){
     //const url = environment.WS_PATH + "pacientes"
-    let url = "";
+    let url = "http://localhost:8080/ppw_final/ws/servicios/findAllHistorialClinico";
     return this.http.get<any> (url);
   }
+
+  findAllCitasByCedula(cedula:any){
+    //const url = environment.WS_PATH + "pacientes"
+    let url = "http://localhost:8080/ppw_final/ws/servicios/HistorialCFindAllByCedula?cedula={String}";
+    return this.http.get<any> (url,cedula);
+  }
   createCitas(hstClinica:hstClinicaWs){
-    let url = "";
+    let url = "http://localhost:8080/ppw_final/ws/servicios/guardarHistorialClinico";
     return this.http.post<any>(url,hstClinica);
   }
 
   readCitas(id:any){
-    let url = "";
+    let url = "http://localhost:8080/ppw_final/ws/servicios/readHistorialClinico?id={int}";
     return this.http.post<any>(url,id);
   }
   updateCitas(hstClinica:hstClinicaWs){
-    let url = "";
+    let url = "http://localhost:8080/ppw_final/ws/servicios/guardarHistorialClinico";
     return this.http.post<any>(url,hstClinica);
   }
 
-  deleteCitas(hstClinica:hstClinicaWs){
-    let url = "";
-    return this.http.post<any>(url,hstClinica);
+  deleteCitas(id:any){
+    let url = "http://localhost:8080/ppw_final/ws/servicios/deleteHistorialClinico?id={int}";
+    return this.http.post<any>(url,id);
   }
 }
